e508ee7c3189d0af8f58491a01921161a5286e33
Carlos Henrique <44164678+colgatepvh@users.noreply.github.com>
2024-01-21 15:17:08 -0400

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "chlspvh-terraform-alura-state"
-    #key    = "Prod/terraform.tfstate"
+    key    = "Prod/terraform.tfstate"
     region = "us-west-2"
   }
 }
